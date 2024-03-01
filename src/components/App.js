3f630ab25acafc043072b7765797c52cc6e7ac9e
Rafaelo <gjergjirafo@gmail.com>
2024-03-01 01:52:02 -0500

    complete-state-events-mini-project

--- src/components/App.js
+++ src/components/App.js
@@ -8,48 +8,42 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
-  const [tasks, setTasks] = useState(TASKS);
-  const categories = CATEGORIES;
-  const [filteredCategory, setFilteredCategory] = useState(null);
 
-  const handleDeleteTask = (deletedTask) => {
-    const filteredTasks = tasks.filter(
-      (task) => deletedTask.text !== task.text
-    );
-    setTasks(filteredTasks);
-  };
+  const [tasks, setTasks] = useState(TASKS) // COPY of TASK, NOT the ORIGINAL TASK
+  const categories = CATEGORIES; // use COPY of CATEGORIES, shit won't work if you use the ORIGINAL CATEGORIES 
+  const [filteredCategory, setFilteredCategory] = useState(null); // filteredCategory is by default a falsy value (more explanation in filteredTasks)
 
-  const onTaskFormSubmit = (newTask) => {
-    setTasks([...tasks, newTask]);
-  };
+  const handleDeleteTask = (deletedTask) => { // for <TaskList>
+    const filteredTasks = tasks.filter(task => deletedTask.text !== task.text); // this one is weird
+    setTasks(filteredTasks); // new tasks = current tasks - deleted task
+  }
 
-  const handleCategoryFilter = (category) => {
-    setFilteredCategory(category);
+  const onTaskFormSubmit = (newTask) => { // for <NewTaskForm>
+    setTasks([...tasks, newTask]); // new tasks = current tasks + submitted task
+  }
+
+  const handleCategoryFilter = (category) => { // for <CategoryFilter>
+    setFilteredCategory(category); // filter category based on button with that category 
   };
 
   let filteredTasks;
-  if (filteredCategory === "All") {
-    filteredTasks = tasks;
-  } else if (filteredCategory) {
-    filteredTasks = tasks.filter((task) => task.category === filteredTasks);
-  } else {
-    filteredTasks = tasks;
+  if (filteredCategory === "All") { // When button All is clicked (filteredCategory is equal to "All")
+    filteredTasks = tasks; // return all tasks without filtering
+  } else if (filteredCategory) { // When a button is clicked (filteredCategory is truthy) - (truthy =/= true, truthy value has a valid value that is not a null or empty)
+    filteredTasks = tasks.filter((task) => task.category === filteredCategory); // return only selected tasks
+  } else { // if none of the button is clicked (default) - (falsy =/= false, falsy value has a value that is null or empty)
+    filteredTasks = tasks; // then all task will show.
   }
 
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter
-        categories={categories}
-        handleCategoryFilter={handleCategoryFilter}
-      />
-      <NewTaskForm
-        categories={categories}
-        onTaskFormSubmit={onTaskFormSubmit}
-      />
+      <CategoryFilter categories={categories} handleCategoryFilter={handleCategoryFilter} />
+      <NewTaskForm categories={categories} onTaskFormSubmit={onTaskFormSubmit} />
       <TaskList tasks={filteredTasks} deleteThis={handleDeleteTask} />
     </div>
   );
 }
 
 export default App;
+
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useState } from "react";
 
-function CategoryFilter({categories, handleCategoryFilter}) {
+function CategoryFilter({categories, handleCategoryFilter}) { // don't miss the {} inside the function
+
+  // const [categories, setCategories] = useState(CATEGORIES)
   const [selectedButton, setSelectedButton] = useState("");
 
   const handleClick = (category) => {
@@ -12,6 +14,7 @@ function CategoryFilter({categories, handleCategoryFilter}) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
+      {/* render <button> elements for each category here */}
       {categories.map(category => (
         <button class={selectedButton === category ? "selected" : ""} key={category} onClick={() => handleClick(category)} >
           {category}
